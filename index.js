f9300bf3cc33a6e7096a14999a5d74674e9b9f8f
Misganaw Desalew <misgandess@gmail.com>
2024-04-08 20:07:08 +0000

    added env file

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyparser=require('body-parser');
 
+app.use(bodyparser.urlencoded({extended:false}));
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post("/api/users",(req,res)=>{
+  const {username}= req.body;
+  
+
+
+});
 
 
 
