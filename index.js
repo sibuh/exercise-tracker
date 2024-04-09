d7bd9a4c36e816e408705a2f633b31b337a7801d
Misganaw Desalew <misgandess@gmail.com>
2024-04-09 14:28:42 +0000

    addded post request handler for exercises

--- index.js
+++ index.js
@@ -25,17 +25,53 @@ app.post("/api/users",async(req,res)=>{
   try {
     const { username} = req.body;
     console.log(username);
-    const user = new User({name:username});
+    const user = new User({username:username});
     await user.save();
 
-    const createdUser=await User.findOne({name:username})
+    const createdUser=await User.findOne({username:username})
 
-    res.json({ '_id':createdUser['_id'],name:createdUser['name']});
+    res.json({ '_id':createdUser['_id'],username:createdUser['username']});
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 
 });
+app.get("/api/users",async (req,res)=>{
+  let result=[]
+  const users= await User.find();
+  for (let user of users){
+    result.push({username: 'sibhat',_id:user['_id']})
+  }
+  console.log("result:",result);
+  res.json(result);
+});
+app.post("/api/users/:id/exercises",async (req,res)=>{
+  const {description,duration,date}=req.body;
+  const userId =req.params;
+  const exercise = new Exercise({
+      userId: userId['id'],
+      description: description,
+      duration: duration,
+      date:date});
+    await exercise.save();
+
+  const user= await User({_id:userId})
+  console.log('user:',user)
+
+  const createdEx=await Exercise({userId:userId['id']});
+  console.log("created exercise:",createdEx);
+  res.json(
+    {
+      username:'sibhat',
+      description:description,
+      duration: duration,
+      date:date,
+      _id: createdEx['_id']
+    })
+
+
+  
+})
 
 
 
